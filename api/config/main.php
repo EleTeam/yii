cd292b3c733b55bf8757af0a44b3f437b3ab4a38
tony <908601756@qq.com>
2016-06-25 13:27:38 +0800

    api

--- api/config/main.php
+++ api/config/main.php
@@ -46,6 +46,10 @@ return [
                     'class' => 'yii\rest\UrlRule',
                     'controller' => ['v1/user'],
                 ],
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => ['v1/category'],
+                ],
             ],
         ],
     ],
